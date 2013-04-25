54adfc705fd32d64506da926ebf948c9170fa71a
Giovanna Russo <russopgiovanna@gmail.com>
2013-04-24 19:52:05 -0430

    Constructor and getCountries method complete

--- class/country.php
+++ class/country.php
@@ -1,12 +1,40 @@
 <?php
+	require_once("data.php");
+
 	class Country{
+
 		//Atributtes
 		protected $_id;
 		protected $_name;
 		protected $_description;
 		protected $_created;
 		
-		//Methods
+		// Constructor
+		public function __construct($row){
+			foreach ($row as $key => $value){
+				switch ($key){
+					case "id":
+						$this->_id = $value;
+						$this->setId($this->_id);
+						break;
+					case "name":
+						$this->_name = $value;
+						$this->setName($this->_name);
+						break;
+					case "description":
+						$this->_description = $value;
+						$this->setDescription($this->_description);
+						break;
+					case "created":
+						$this->_created = $value;
+						break;
+				}
+			}
+
+		}
+		
+
+		//Set & Get Methods
 		public function getId(){
 			return $this->_id;
 		}
@@ -20,7 +48,7 @@
 		}
 		
 		public function setName($name){
-			$this->name = $name;
+			$this->_name = $name;
 		}
 		
 		public function getDescription(){
@@ -28,8 +56,30 @@
 		}
 		
 		public function setDescription($description){
-			$this->description = $description;
+			$this->_description = $description;
 		}
 	
+		// Methods 
+
+		public static function getCountries(){
+			$conn = Data::connect();
+			$sql = "SELECT * FROM countries";
+			try{
+				$st= $conn->prepare($sql);
+				$st->execute();
+				$countries = array();
+				foreach ($st->fetchAll() as $row) {
+					$countries[] = new Country ($row);
+				}
+				Data::disconnect();
+				return $countries;
+			} catch (PDOException $e) {
+				Data::disconnect();
+				die("Query failed: " . $e->getMessage());
+			}
+		}
 	}
+
+	echo print_r(Country::getCountries());
+
 ?>
